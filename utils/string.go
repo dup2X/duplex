050397241652db13cf1dec1f694504291a814d0f
dup2X <dup2snow@gmail.com>
2016-06-06 14:04:12 +0800

    bytes to string

---
+++ utils/string.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+func String(data []byte) (str string) {
+	if len(data) == 0 {
+		return ""
+	}
+
+	ptrBytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	ptrString := (*reflect.SliceHeader)(unsafe.Pointer(&str))
+	ptrString.Data = ptrBytes.Data
+	ptrString.Len = ptrBytes.Len
+	return
+}
